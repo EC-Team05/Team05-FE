d802c46802ae189312d1eec34957ef786fb0ac79
quynhnhu-512000 <dangnguyenquynhnhu512000@gmail.com>
2021-08-06 09:36:38 +0700

    NhuDang

--- src/_components/header/Header.js
+++ src/_components/header/Header.js
@@ -115,9 +115,6 @@ class Header extends React.Component {
 									<li className={`${pathname === ('/Payment-Guide') ? 'active' : ''}`}>
 										<Link to = "/Payment-Guide">Hướng dẫn thanh toán</Link>
 									</li>
-									<li className={`${pathname === ('/booking-stylist') ? 'active' : ''}`}>
-										<Link to = "/booking-stylist">Booking stylist</Link>
-									</li>
 								</ul>
 							</li>
 							<li className={pathname === ('/blog-list') ? 'active' : ''}>
